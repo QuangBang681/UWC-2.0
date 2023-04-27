262fc327b7bc21495c1e4612af42cab6d5c9de33
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2023-04-27 12:59:52 +0700

    + add more vehicle info

--- Web/src/app/models/vehicle.js
+++ Web/src/app/models/vehicle.js
@@ -6,6 +6,10 @@ const VehicleSchema = new mongoose.Schema(
     {
         type: {type: String, require: true, unique: true},
         assign_empolyee: {type: ObjectId, unique: true, ref: 'Employee'},
+        weight_capacity: {type: Number, require: true},
+        volume_capacity: {type: Number, require: true},
+        fuel_consumption: {type: Number, require: true}
+
     },
     {collection: 'vehicle'}
 );
